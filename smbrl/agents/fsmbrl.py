5a3d72d53d0d0afa949b36a388b696e45fb0e2eb
Yarden As <yarden.as@inf.ethz.ch>
2023-05-12 11:54:54 +0200

    Sadam work (#3)
    * Make SMBRL more RL ready
    
    * Works on single task, fails on multi. Tested also on compare script with gru and it seems to fail. Further investigation required

--- smbrl/agents/fsmbrl.py
+++ smbrl/agents/fsmbrl.py
@@ -1,15 +1,16 @@
 from typing import Iterator
 
 import equinox as eqx
+import jax
 import jax.numpy as jnp
 import numpy as np
 from gymnasium import spaces
 from omegaconf import DictConfig
 
+from smbrl.agents import cem
 from smbrl.agents import model_learning as ml
-from smbrl.agents import smbrl
 from smbrl.agents.base import AgentBase
-from smbrl.agents.models import S4Model
+from smbrl.agents.models import SSM, S4Model
 from smbrl.logging import TrainingLogger
 from smbrl.metrics import MetricsAccumulator
 from smbrl.replay_buffer import ReplayBuffer
@@ -18,6 +19,45 @@ from smbrl.types import FloatArray
 from smbrl.utils import Count, Learner, add_to_buffer, normalize
 
 regression_step = eqx.filter_jit(ml.regression_step)
+
+
+@eqx.filter_jit
+def update_state(
+    model: S4Model,
+    ssm: list[SSM],
+    prev_hidden: list[jax.Array],
+    observation: jax.Array,
+    action: jax.Array,
+) -> list[jax.Array]:
+    step = lambda o, a, h: model.step(o, a, ssm, h)
+    hidden = jax.vmap(step)(observation, action, prev_hidden)[0]
+    return hidden
+
+
+@eqx.filter_jit
+def policy(
+    model: S4Model,
+    ssm: list[SSM],
+    prev_hidden: list[jax.Array],
+    observation: jax.Array,
+    horizon: int,
+    init_guess: jax.Array,
+    key: jax.random.KeyArray,
+    cem_config: cem.CEMConfig,
+) -> jax.Array:
+    # vmap over observations and hiddens states for each task in the batch.
+    cem_per_env = jax.vmap(
+        lambda o, h: cem.policy(
+            o,
+            bind_to_model(model.sample, layers_hidden=h, layers_ssm=ssm),
+            horizon,
+            init_guess,
+            key,
+            cem_config,
+        )
+    )
+    action = cem_per_env(observation, prev_hidden)
+    return action
 
 
 class fSMBRL(AgentBase):
@@ -60,7 +100,7 @@ class fSMBRL(AgentBase):
             (config.training.parallel_envs, config.agent.plan_horizon)
             + action_space.shape
         )
-        self.s4_state = self.model.init_state
+        self.s4_state = init_state(self.model, self.config.training.task_batch_size)
         self.ssm = self.model.ssm
 
     def __call__(
@@ -75,18 +115,22 @@ class fSMBRL(AgentBase):
             )
             horizon = self.config.agent.plan_horizon
             init_guess = jnp.zeros((horizon, self.replay_buffer.action.shape[-1]))
-            action = smbrl.policy(
+            action = policy(
+                self.model,
+                self.ssm,
+                self.s4_state,
                 normalized_obs,
-                bind_to_model(
-                    self.model.sample, layers_ssm=self.ssm, layers_hidden=self.s4_state
-                ),
                 horizon,
                 init_guess,
                 next(self.prng),
                 self.config.agent.cem,
             )
             self.plan = np.asarray(action)
-        return self.plan[:, self.replan.count]
+        action = self.plan[:, self.replan.count]
+        self.s4_state = update_state(
+            self.model, self.ssm, self.s4_state, observation, action
+        )
+        return action
 
     def observe(self, trajectory: TrajectoryData) -> None:
         self.obs_normalizer.update_state(
@@ -103,6 +147,7 @@ class fSMBRL(AgentBase):
             self.config.training.scale_reward,
         )
         self.update_model()
+        self.s4_state = init_state(self.model, self.config.training.task_batch_size)
 
     def update_model(self):
         for batch in sample_data(self.replay_buffer, self.config.agent.update_steps):
@@ -113,6 +158,10 @@ class fSMBRL(AgentBase):
                 self.model_learner.state,
             )
         self.logger["agent/model/loss"] = float(loss.mean())
+        self.ssm = self.model.ssm
+
+    def reset(self):
+        pass
 
 
 def bind_to_model(fn, **kwargs):
@@ -124,3 +173,7 @@ def sample_data(
 ) -> Iterator[TrajectoryData]:
     for batch in replay_buffer.sample(n_batches):
         yield ml.prepare_data(batch)
+
+
+def init_state(model, batch_size):
+    return list(map(lambda x: np.tile(x, (batch_size, 1, 1)), model.init_state))
