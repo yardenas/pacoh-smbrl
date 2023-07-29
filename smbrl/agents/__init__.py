1c23df62e475c7c0863fe0a68f08c9d8b52a6e44
Yarden <yarden.as@inf.ethz.ch>
2023-07-29 13:37:18 +0200

    Make tests pass

--- smbrl/agents/__init__.py
+++ smbrl/agents/__init__.py
@@ -1,7 +1,6 @@
 from gymnasium.spaces import Box
 from omegaconf import DictConfig
 
-from smbrl.agents.asmbrl import ASMBRL
 from smbrl.agents.mmbrl import MMBRL
 from smbrl.agents.smbrl import SMBRL
 from smbrl.logging import TrainingLogger
@@ -17,8 +16,6 @@ def make(
     match config.agent.name:
         case "smbrl":
             return SMBRL(observation_space, action_space, config, logger)
-        case "asmbrl":
-            return ASMBRL(observation_space, action_space, config, logger)
         case "mmbrl":
             return MMBRL(observation_space, action_space, config, logger)
         case _:
