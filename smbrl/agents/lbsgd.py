9c04de4d8a25a7920a4c946c8a8d78237bfcabbc
Yarden As <yarden.as@inf.ethz.ch>
2023-08-28 21:55:41 +0200

    New implementation of lbsgd

--- smbrl/agents/lbsgd.py
+++ smbrl/agents/lbsgd.py
@@ -1,54 +1,19 @@
 """https://github.com/lasgroup/lbsgd-rl/blob/main/lbsgd_rl/"""
 
-from typing import Any, NamedTuple
+from typing import Any, Callable, NamedTuple
 
 import equinox as eqx
 import jax
 import jax.numpy as jnp
-import optax
 from jaxtyping import PyTree
-from optax._src import base
 
-from smbrl.utils import Learner
+from smbrl.agents import actor_critic as ac
+from smbrl.agents.safe_actor_critic import ActorEvaluation
+from smbrl.utils import pytrees_unstack
 
 
 class LBSGDState(NamedTuple):
     eta: jax.Array
-    lr: jax.Array
-
-
-def scale_by_lbsgd(
-    eta: float, m_0: float, m_1: float, eta_rate: float, base_lr: float
-) -> base.GradientTransformation:
-    eta_rate += 1.0
-
-    def init_fn(params):
-        del params
-        return LBSGDState(eta, base_lr)
-
-    def update_fn(updates, state, params=None):
-        del params
-
-        def happy_case():
-            lr = compute_lr(constraint, loss_grads, constraints_grads, m_0, m_1, eta_t)
-            new_eta = eta_t / eta_rate
-            updates = jax.tree_map(lambda x: x * lr, loss_grads)
-            return updates, LBSGDState(new_eta, lr)
-
-        def fallback():
-            # Taking the negative gradient of the constraints to minimize the costs
-            updates = jax.tree_map(lambda x: x * -1.0, constraints_grads)
-            return updates, LBSGDState(eta, 1.0)
-
-        loss_grads, constraints_grads, constraint = updates
-        eta_t = state.eta
-        return jax.lax.cond(
-            jnp.greater(constraint, 0.0),
-            happy_case,
-            fallback,
-        )
-
-    return base.GradientTransformation(init_fn, update_fn)
 
 
 def compute_lr(constraint, loss_grads, constraint_grads, m_0, m_1, eta):
@@ -61,7 +26,7 @@ def compute_lr(constraint, loss_grads, constraint_grads, m_0, m_1, eta):
             2.0 * jnp.abs(projection) / jnp.linalg.norm(loss_grads)
             + jnp.sqrt(constraint * m_1 + 1e-8)
         )
-        / jnp.linalg.norm(loss_grads)
+        / (jnp.linalg.norm(loss_grads) + 1e-8)
     )
     m_2 = (
         m_0
@@ -71,24 +36,70 @@ def compute_lr(constraint, loss_grads, constraint_grads, m_0, m_1, eta):
         * jnp.linalg.norm(projection) ** 2
         / ((jnp.linalg.norm(loss_grads) * constraint) ** 2)
     )
-    rhs = 1.0 / (m_2 + 1e-8)
+    rhs = 1.0 / m_2
     return jnp.minimum(lhs, rhs)
 
 
-class LBSGDLearner(Learner):
-    def __init__(
+def lbsgd_update(
+    state: LBSGDState, updates: PyTree, eta_rate: float, m_0: float, m_1: float
+) -> tuple[PyTree, LBSGDState]:
+    def happy_case():
+        lr = compute_lr(constraint, loss_grads, constraints_grads, m_0, m_1, eta_t)
+        new_eta = eta_t / eta_rate
+        updates = jax.tree_map(lambda x: x * lr, loss_grads)
+        return updates, LBSGDState(new_eta, lr)
+
+    def fallback():
+        # Taking the negative gradient of the constraints to minimize the costs
+        updates = jax.tree_map(lambda x: x * -1.0, constraints_grads)
+        return updates, LBSGDState(eta_t, 1.0)
+
+    loss_grads, constraints_grads, constraint = updates
+    eta_t = state.eta
+    return jax.lax.cond(
+        jnp.greater(constraint, 0.0),
+        happy_case,
+        fallback,
+    )
+
+
+def jacrev(f, has_aux=False):
+    def jacfn(x):
+        y, vjp_fn, aux = eqx.filter_vjp(f, x, has_aux=has_aux)
+        (J,) = eqx.filter_vmap(vjp_fn, in_axes=0)(jnp.eye(len(y)))
+        return J, aux
+
+    return jacfn
+
+
+class LBSGDPenalizer:
+    def __init__(self, m_0, m_1, eta_rate) -> None:
+        self.m_0 = m_0
+        self.m_1 = m_1
+        self.eta_rate = eta_rate
+        self.state = None
+
+    def __call__(
         self,
-        model: PyTree,
-        optimizer_config: dict[str, Any],
-        eta: float,
-        m_0: float,
-        m_1: float,
-        eta_rate: float,
-        base_lr: float,
-    ):
-        self.optimizer = optax.chain(
-            scale_by_lbsgd(eta, m_0, m_1, eta_rate, base_lr),
-            optax.scale_by_adam(eps=optimizer_config.get("eps", 1e-8)),
-            optax.scale(-base_lr),
+        evaluate: Callable[[ac.ContinuousActor], ActorEvaluation],
+        state: Any,
+        actor: ac.ContinuousActor,
+    ) -> tuple[PyTree, Any, ActorEvaluation, dict[str, float]]:
+        def evaluate_helper(actor):
+            evaluation = evaluate(actor)
+            outs = jnp.stack([-evaluation.objective, evaluation.constraint])
+            return outs, evaluation
+
+        jacobian, rest = jacrev(evaluate_helper, has_aux=True)(actor)
+        loss_grads, constraint_grads = pytrees_unstack(jacobian)
+        updates, state = lbsgd_update(
+            state,
+            (loss_grads, constraint_grads, rest.constraint),
+            self.eta_rate,
+            self.m_0,
+            self.m_1,
         )
-        self.state = self.optimizer.init(eqx.filter(model, eqx.is_array))
+        metrics = {
+            "agent/lbsgd/eta": state.eta.item(),
+        }
+        return updates, state, rest, metrics
